3f1fb21ee7a05b1a68271d3b684dab2afdf1b9d5
Epyc <advay.rc@gmail.com>
2021-12-31 17:47:11 -0500

    Updated UI

--- Main.py
+++ Main.py
@@ -18,7 +18,7 @@ options = ["Rock", "Paper", "Sizzors"]
 
 #Defines the input function
 def check():
-    move = input("Opponents move: ")
+    move = input("What was your move?: ")
     oMove = move.lower()
     if not(oMove == "r" or "p" or "s"):
         print("Invalid input")
@@ -30,23 +30,26 @@ def check():
 pWins = 0
 cWins = 0
 for i in range(rounds):
+    a = input("Press enter to continue:")
     output = npr.choice(options, p = Controller.appendProbabilities())
+    print(output)
     move = check()
-    if move == "p" and output == "r":
+    o1 = list(output.lower())
+    osimplified = o1[0]
+    if (move == "p") and (output == "Rock"):
         pWins += 1
         print("You Won!")
-    elif move == "s" and output == "p":
+    elif (move == "s") and (output == "Paper"):
         pWins += 1
         print("You Won!")
-    elif move == "r" and output == "s":
+    elif (move == "r") and (output == "Sizzors"):
         pWins += 1
         print("You Won!")
-    elif  move == output:
+    elif  move == osimplified:
         print("tie")
     else:
         cWins += 1
         print("You Lost")
-    print(output)
     Controller.adjustProbabilities(move)
 
 print('''OVERALL RESULTS:
